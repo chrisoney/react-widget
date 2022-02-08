6747bfb808b87209424531e4c97fa6a5d455f0b7
chrisoney <christopher.oney@yahoo.com>
2022-02-07 18:12:47 -0800

    testing seeder with jsonb records

---
+++ backend/db/seeders/20220208020844-insertMaps.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { User } = require('../models');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+      */
+    const userChris = await User.findOne({ where: {username: 'chris'} });
+    return queryInterface.bulkInsert('Maps', [
+      {
+        userId: userChris.id
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
