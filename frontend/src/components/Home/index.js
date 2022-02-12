2f561910cef237ff84047df29ea9ae329b870496
chrisoney <christopher.oney@yahoo.com>
2022-02-12 15:51:56 -0800

    very basic widget choice page

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './home.module.css';
 
 const Home = () => {
   return (
-    <>
-    </>
+    <div className={styles.widget_choice_container}>
+      <Link to='/maps'>Maps</Link>
+    </div>
   )
 }
 
